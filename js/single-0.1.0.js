566fff32bd5c36e3ebfe2dd390011d2241ba8402
Rafael Fragoso <rafaelfragosom@gmail.com>
2015-08-14 11:38:53 -0300

    Refactoring code and starting to implement best practices

--- js/single-0.1.0.js
+++ js/single-0.1.0.js
@@ -4,138 +4,127 @@
 
 		var opts = $.extend({}, $.fn.single.defaults, options);
 
+		var changeCSS = function(element) {
+			// Grab the screen resolution
+			var windowWidth 	= "100%";
+			var windowHeight	= $(window).height();
+			
+			// Count how many targets the div has
+			var targetsSize		= $(".single > div").size();
+
+			// Resize the parent div
+			$(element).css({
+				"width" : windowWidth,
+				"height": windowHeight * targetsSize
+			});
+
+			// Resize all the targets div
+			$(element).children(".single > div").each(function(){
+				$(this).css({
+					"width" : windowWidth,
+					"height": windowHeight
+				});
+			});
+		}
+
+		// function to resize the images
+		var changeIMG = function() {
+			// Grab the screen resolution
+			windowWidth = $(window).width();
+			
+			$("img.single-responsive").each(function(index, element){
+				src				= $(element).attr('src');
+				imgName		= "";
+				imgFinal	= "";
+				imgSplit	= {};
+				imagePrefix	= checkResolution(windowWidth);
+
+				if ( src.match("/") ) { // Match if there's a full URL at the IMG src and cut it
+					re 	= new RegExp(".*\/(.*)$");
+					m 	= re.exec(src);    
+					imgName = m[1];
+				} else {
+					// Just the img without an URL
+					imgName = src;
+				}
+
+				if( imgName.match(/\-\w+/) ) {
+
+					src = src.replace(/\-\w+/, imagePrefix);
+
+				} else {
+
+					// Split the name of the extension
+					imgSplit 	= imgName.split('.'); 
+
+					// Replace the name with the image prefix
+					imgFinal	= imgSplit[0] + imagePrefix + '.' + imgSplit[1];
+					src 		= src.replace(imgName, imgFinal);
+
+				}
+
+				// Replace the image
+				$(element).attr('src', src);
+			});
+		}
+
+		// function to scroll the page to a section
+		var goToSection = function(link) {
+
+			var goingTo 		    = $(link).attr('data-link'); // get the data-link value
+			var targetPosition 	= $('[data-target="'+goingTo+'"]').position().top; // get the position of the target
+
+			// jQuery Easing animation
+			$("html, body").animate({
+				scrollTop: targetPosition
+			}, {
+				duration: opts.speed,
+				easing: opts.animation
+			});
+
+		}
+
+		// function to check the resolution and return the prefix for the image
+		var checkResolution = function(windowWidth) {
+			if (windowWidth <= 480) {
+				return opts.sufixes.smallest;
+			} 
+			else if(windowWidth > 480 && windowWidth <= 767) {
+				return opts.sufixes.small;
+			} 
+			else if(windowWidth > 767 && windowWidth <= 979) {
+				return opts.sufixes.medium;
+			} 
+			else {
+				return '';
+			}
+		}
+
 		return this.each(function(){
 
 			// Get the instance
 			var element = $(this);
 
-			// Resize the "data-target" divs
+			// Resize the divs
 			changeCSS(element);
-			// Resize all the "data-img='true'" images
-			changeIMG(opts);
+
+			// Resize all the images
+			changeIMG();
 
 			// Bind the methods changeCSS and changeIMG to the resize window event
 			$(window).bind("resize", function(){  
 				changeCSS(element);  
-				changeIMG(opts);
+				changeIMG();
 			});
 
 			// Bind the method click to the data-link
 			$("[data-link]").bind("click", function(event){  
 				event.preventDefault();
-				goToSection(this, opts);  
+				goToSection(this);  
 			});
 
 		});
-
 	};
-
-	// function to resize all the "data-target" divs
-	function changeCSS(element) {
-
-		// Grab the screen resolution
-		var windowWidth 	= "100%";
-		var windowHeight	= $(window).height();
-		// Count how many targets the div has
-		var targetsSize		= $("[data-target]").size();
-
-		// Resize the parent div
-		$(element).css({
-			"width" : windowWidth,
-			"height": windowHeight * targetsSize
-		});
-
-		// Resize all the targets div
-		$(element).children("div[data-target]").each(function(){
-			$(this).css({
-				"width" : windowWidth,
-				"height": windowHeight
-			});
-		});
-
-	}
-
-	// function to resize the images
-	function changeIMG(opts) {
-
-		// Grab the screen resolution
-		windowWidth 	= $(window).width();
-		
-		$("img[data-img='true']").each(function(index, element){
-
-			src			= $(element).attr('src');
-			imgName		= "";
-			imgFinal	= "";
-			imgSplit	= {};
-			imagePrefix	= checkResolution(windowWidth, opts);
-
-			if ( src.match("/") ) { // Match if there's a full URL at the IMG src and cut it
-				re 	= new RegExp(".*\/(.*)$");
-				m 	= re.exec(src);    
-				imgName = m[1];
-			} else {
-				// Just the img without an URL
-				imgName = src;
-			}
-
-			if( imgName.match(/\-\w+/) ) {
-
-				src = src.replace(/\-\w+/, imagePrefix);
-
-			} else {
-
-				// Split the name of the extension
-				imgSplit 	= imgName.split('.'); 
-
-				// Replace the name with the image prefix
-				imgFinal	= imgSplit[0] + imagePrefix + '.' + imgSplit[1];
-				src 		= src.replace(imgName, imgFinal);
-
-			}
-
-			// Replace the image
-			$(element).attr('src', src);
-
-		});
-
-	}
-
-	// function to scroll the page to a section
-	function goToSection(link, opts) {
-
-		var goingTo 		= $(link).attr('data-link'); // get the data-link value
-		var targetPosition 	= $('[data-target="'+goingTo+'"]').position().top; // get the position of the target
-
-		// jQuery Easing animation
-		$("html, body").animate({
-			scrollTop: targetPosition
-		}, {
-			duration: opts.speed,
-			easing: opts.animation
-		});
-
-	}
-
-	// function to check the resolution and return the prefix for the image
-	function checkResolution(windowWidth, opts) {
-
-		if (windowWidth <= 480) {
-			return opts.sufixes.smallest;
-		} 
-
-		if(windowWidth > 480 && windowWidth <= 767) {
-			return opts.sufixes.small;
-		} 
-
-		if(windowWidth > 767 && windowWidth <= 979) {
-			return opts.sufixes.medium;
-		} 
-
-		if(windowWidth > 979) {
-			return opts.sufixes.normal;
-		}
-
-	}
 
 	// Plugin defaults
 	$.fn.single.defaults = {
@@ -145,7 +134,6 @@
 			smallest: "-smallest",
 			small   : "-small",
 			medium  : "-medium",
-			normal  : "" // Leave blank for no prefix
 		}
 	};
 
